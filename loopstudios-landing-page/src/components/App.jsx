03bfb2ab8c449cd749e427b5c07db5d46ddab136
Raj Aryan <rj.rajeryan0071@gmail.com>
2021-08-14 22:56:27 +0530

    react app initiated

---
+++ loopstudios-landing-page/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDom from "react-dom";
+
+export default function App() {
+    return (
+        <div className="root">
+            <h1>Loop studios landing page</h1>
+        </div>
+    )
+}
