b0db2b45c812d4e3736e067939419e5f4b436bd4
Raj Aryan <rj.rajeryan0071@gmail.com>
2021-08-20 23:50:28 +0530

    header-section completed

--- loopstudios-landing-page-react/src/components/Header.jsx
+++ loopstudios-landing-page-react/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import logo from './logo.svg'
-import menuIcon from './icon-hamburger.svg'
+import CloseIcon from '@material-ui/icons/Close';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -22,31 +22,60 @@ const useStyles = makeStyles((theme) => ({
 export default function Headers() {
   const classes = useStyles();
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
+  const header = useRef(null);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const openMenu = useRef(null);
+  const closeMenu = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    const displayWidth = header.current.getBoundingClientRect().width;
+    // console.log(typeof(displayWidth));
+
+    if (showLinks && displayWidth <= 376) {
+      linksContainerRef.current.style.height = `max-content`;
+      openMenu.current.style.visibility = 'hidden'
+      closeMenu.current.style.visibility = 'visible'
+      linksRef.current.style.visibility = 'visible'
+    } if(!showLinks && displayWidth <= 376) {
+      linksContainerRef.current.style.height = '0';
+      openMenu.current.style.visibility = 'visible'
+      closeMenu.current.style.visibility = 'hidden'
+      linksRef.current.style.visibility = 'hidden'
+    }
+  }, [showLinks]);
 
   return (
-    <div className="header">
+    <div className="header" ref={header}>
 
       <nav>
-        <div className={classes.root}>
+        <div className={classes.root} id="nav-div">
         <AppBar position="static" className="nav-center">
           <Toolbar variant="dense">
 
-            <IconButton edge="start" className={`${classes.menuButton} "menu-icon"`} color="inherit" aria-label="menu">
-              <MenuIcon />
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" id="menu-div">
+              <MenuIcon id="menu-icon" onClick={toggleLinks} ref={openMenu} />
+              <CloseIcon id="close-icon" onClick={toggleLinks} ref={closeMenu}/>
             </IconButton>
 
             <Typography variant="h6" color="inherit">
               <img src={logo} alt="" />
             </Typography>
 
-            <div className="menu-items">
-              <ul>
-                <li><a href="#">About</a><hr/></li>
-                <li><a href="#">Careers</a><hr/></li>
-                <li><a href="#">Events</a><hr/></li>
-                <li><a href="#">Products</a><hr/></li>
-                <li><a href="#">Supporrt</a><hr/></li>
+            <div className="menu-items" ref={linksContainerRef}>
+              <ul ref={linksRef}>
+                <span>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Support</a></li>
+                </span>
               </ul>
             </div>
           </Toolbar>
@@ -54,13 +83,19 @@ export default function Headers() {
         </div>
       </nav>
 
-      <motion.div className="test" 
+      {/* <motion.div className="test" 
         layout
         transition={{duration: 0.25}}
         data-isOpen={isOpen}
         initial={{ borderRadius: 50 }}
         // final={{ borderRadius: 50 }}
-        onClick={() => setIsOpen(!isOpen)}></motion.div>
+        onClick={() => setIsOpen(!isOpen)}
+      ></motion.div> */}
+
+      <div className="banner-text">
+        <h1>IMMERSIVE EXPERIENCES THAT DELIVER</h1>
+      </div>
+      
     </div>
   );
 }
